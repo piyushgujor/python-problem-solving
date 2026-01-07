d6d50e6219d3416d24b5fde24b1ee598f29d6b29
Piyush Gujor <piyushgujor678@gmail.com>
2026-01-07 20:01:52 +0530

    Create fizzbuzz_plus.py

---
+++ 01_basics/01_basics/fizzbuzz_plus.py
@@ -0,0 +1,6 @@
+#Print "Fizz" if the number is divisible by 3
+def cheak_divisible(number):
+if(Number%3==0):
+             return The Number is Divisible by 3
+else:
+             return The Number is not Divisible by 3
